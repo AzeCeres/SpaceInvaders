9e4916ffbb148155a66419227cf898e14c25d95f
AndreasStokvik <111863574+AndreasStokvik@users.noreply.github.com>
2023-03-05 20:30:47 +0100

    Imported code from TA repo in GameModeBase

--- Source/SpaceInvaders/SpaceInvadersGameModeBase.h
+++ Source/SpaceInvaders/SpaceInvadersGameModeBase.h
@@ -6,12 +6,60 @@
 #include "GameFramework/GameModeBase.h"
 #include "SpaceInvadersGameModeBase.generated.h"
 
+class AE_Enemy;
 /**
- * 
+ *
  */
 UCLASS()
 class SPACEINVADERS_API ASpaceInvadersGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ASpaceInvadersGameModeBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		TArray<float> WaveDifficulty;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		TArray<int> WaveSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		TArray<int> WaveSpawnFrequency;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MinX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MaxX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MinY;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MaxY;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int CurrentWave;
+
+	/** Blueprints: we set these in our blueprint */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		TSubclassOf<AE_Enemy> E_Enemy_BP;
+
+private:
+	int LeftToSpawn;
+	float Clock;
+	bool GameWon;
+
+
+	void ChangeWave(int wave);
+
 };
